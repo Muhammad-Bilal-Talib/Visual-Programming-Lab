adc4614d942d35ac91f1d3a9dd15dff336f748de
Muhammad-Bilal-Talib <233543@students.au.edu.pk>
2024-10-16 15:07:47 +0500

    inhertance done
    files uploaded

--- Lab-1/Program.cs
+++ Lab-1/Program.cs
@@ -1,108 +1,145 @@
 ﻿using System;
+using System.Linq;
 
-namespace Ex_Task
+namespace act3
 {
-    class Program1
+    
+
+    public class WorkItem
+    {
+        private static int currentID;
+
+        protected int ID { get; set; }
+
+        protected string Title { get; set; }
+
+        protected string Description { get; set; }
+
+        protected TimeSpan jobLength { get; set; }
+
+        public WorkItem()
+        {
+            ID = 0;
+            Title = "Default Title";
+            Description = "Default Description";
+            jobLength = new TimeSpan();
+        }
+
+        public WorkItem(string tit, string desc, TimeSpan joblen)
+        {
+            this.ID = GetNextID();
+            this.Title = tit;
+            this.Description = desc;
+            this.jobLength = joblen;
+        }
+
+        static WorkItem()
+        {
+            currentID = 0;
+        }
+
+        protected int GetNextID()
+        {
+            return ++currentID;
+        }
+
+        public void update(string titl, TimeSpan joblen)
+        {
+            this.Title = titl;
+            this.jobLength = joblen;
+        }
+
+        public override string ToString()
+        {
+              return String.Format($"{0}-{1}",this.ID,this.Title);
+        }
+
+
+    }
+
+    public class ChangeRequest : WorkItem
+    {
+        protected int originalItemID { get; set; }
+
+        // Constructors. Because neither constructor calls a base-class
+        // constructor explicitly, the default constructor in the base class
+        // is called implicitly. The base class must contain a default
+        // constructor.
+
+        // Default constructor for the derived class.
+        public ChangeRequest() { }
+
+        // Instance constructor that has four parameters.
+        public ChangeRequest(string title, string desc, TimeSpan jobLen, int originalID)
+        {
+            // The following properties and the GetNexID method are inherited
+            // from WorkItem.
+            this.ID = GetNextID();
+            this.Title = title;
+            this.Description = desc;
+            this.jobLength = jobLen;
+
+            // Property originalItemId is a member of ChangeRequest, but not
+            // of WorkItem.
+            this.originalItemID = originalID;
+        }
+
+        public ChangeRequest(int originalItemID)
+        {
+            this.originalItemID = originalItemID;
+        }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1. Sum");
-            Console.WriteLine("2. Subtract");
-            Console.WriteLine("3. Multiplication");
-            Console.WriteLine("4. Division");
-            Console.WriteLine("5. Exit");
+            //result = SafeDivision(a, b);
 
-            char ch;
-            do
-            {
-                Console.Write("Enter Your Choice:");
-                string choice = Console.ReadLine();
-                ch = char.Parse(choice);
+            //Console.WriteLine($"{a} divided by {b} = {result}");
 
-                switch (ch)
-                {
-                    case '1':
-                        sum();
-                        break;
-                    case '2':
-                        subtract();
-                        break;
-                    case '3':
-                        multiply();
-                        break;
-                    case '4':
-                        division();
-                        break;
-                    case '5':
-                        break;
+            //TestCatch2();
 
-                }
-            } while (ch != '5');
+            // Create an instance of WorkItem by using the constructor in the
+            // base class that takes three arguments.
 
+            WorkItem item = new WorkItem("Fix Bugs", "Fix all bugs in my code branch", new TimeSpan(3, 4, 0, 0));
 
+            // Create an instance of ChangeRequest by using the constructor in
+            // the derived class that takes four arguments.
+
+            ChangeRequest change = new ChangeRequest("Change Base Class Design", "Add members to the class", new TimeSpan(4, 0, 0), 1);
+
+            // Use the ToString method defined in WorkItem.
+
+            Console.WriteLine(item.ToString());
+
+            // Use the inherited Update method to change the title of the
+            // ChangeRequest object.
+
+            change.update("Change the Design of the Base Class", new TimeSpan(4, 0, 0));
+
+            // ChangeRequest inherits WorkItem's override of ToString.
+
+            Console.WriteLine(change.ToString());
+
+            // Keep the console open in debug mode.
+
+            Console.WriteLine("Press any key to exit.");
+
+            Console.ReadKey();
         }
-
-        private static void sum()
-        {
-            Console.Write("Enter a Value:");
-            string num1 = Console.ReadLine();
-            Console.Write("Enter a Value:");
-            string num2 = Console.ReadLine();
-
-            int a = int.Parse(num1);
-            int b = int.Parse(num2);
-
-
-            Console.WriteLine($"Sum of {num1} and {num2} is:{a + b}\n\n");
-
-
-        }
-        private static void subtract()
-        {
-            Console.Write("Enter a Value:");
-            string num1 = Console.ReadLine();
-            Console.Write("Enter a Value:");
-            string num2 = Console.ReadLine();
-
-            int a = int.Parse(num1);
-            int b = int.Parse(num2);
-
-
-            Console.WriteLine($"Sum of {num1} and {num2} is:{a - b}\n\n");
-
-
-        }
-        private static void multiply()
-        {
-            Console.Write("Enter a Value:");
-            string num1 = Console.ReadLine();
-            Console.Write("Enter a Value:");
-            string num2 = Console.ReadLine();
-
-            int a = int.Parse(num1);
-            int b = int.Parse(num2);
-
-
-            Console.WriteLine($"Sum of {num1} and {num2} is:{a * b}\n\n");
-
-
-        }
-        private static void division()
-        {
-            Console.Write("Enter a Value:");
-            string num1 = Console.ReadLine();
-            Console.Write("Enter a Value:");
-            string num2 = Console.ReadLine();
-
-            int a = int.Parse(num1);
-            int b = int.Parse(num2);
-
-
-            Console.WriteLine($"Sum of {num1} and {num2} is:{a / b}\n\n");
-
-
-        }
-
     }
+
+
+
+
+
+       
+
+
+
+
 }
+
 
