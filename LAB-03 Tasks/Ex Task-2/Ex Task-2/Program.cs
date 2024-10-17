264a950bae9fa368e9da9554ebd368dc725f2856
Muhammad-Bilal-Talib <233543@students.au.edu.pk>
2024-10-17 14:58:17 +0500

    Lab-03
    Files uploaded

---
+++ LAB-03 Tasks/Ex Task-2/Ex Task-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex_Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square();
+        }
+
+        private static void Square()
+        {
+            for(int i=1;i<=10;i++)
+            {
+                int n = i * i;
+                Console.WriteLine($"Square of {i} is {n}");
+            }
+
+        }
+    }
+}
