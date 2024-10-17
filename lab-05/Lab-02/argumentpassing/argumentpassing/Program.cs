3aea81273432d092079315aa70022d23bf49199a
Muhammad-Bilal-Talib <233543@students.au.edu.pk>
2024-10-17 14:45:15 +0500

    Lab-02
    Uploaded files

---
+++ lab-05/Lab-02/argumentpassing/argumentpassing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace argumentpassing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length>0)
+            {
+                Console.WriteLine(args[0]);
+                Console.WriteLine(args[1]);
+                Console.WriteLine(args[2]);
+            }
+        }
+    }
+}
---
+++ lab-05/Lab-02/task-1 lab-02/Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Name: " + "Bilal Talib");
+            Console.WriteLine("Registration No: " + "18977453426");
+            Console.WriteLine("Address: " + "Multan");
+        }
+    }
+}
