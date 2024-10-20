61ca9beac0955dde18824e3ef9682031c48972c7
Muhammad-Bilal-Talib <233543@students.au.edu.pk>
2024-10-20 12:06:04 +0500

    Add files via upload

---
+++ lab-05/LAB-03 Tasks/Ex Task-5 Lab-3/Ex Task-5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Findevenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value to check Even Odd:");
+            string getval = Console.ReadLine();
+            int num1 = int.Parse(getval);
+
+            checknum(num1);
+        }
+
+        private static void checknum(int num)
+        {
+            if (num%2==0)
+            {
+                Console.WriteLine($"{num} is even");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is odd");
+            }
+
+        }
+    }
+
+}
---
+++ lab-05/LAB-03 Tasks/Ex Task-6 Lab-3/Ex Task-6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Findaverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            average();
+        }
+
+        private static void average()
+        {
+            Console.Write("Number of Students are:");
+            string num = Console.ReadLine();
+            int n = int.Parse(num);
+            int sum = 0;
+
+            for (int i = 1; i<=n; i++)
+            {
+                Console.Write($"Enter marks of Students {i}:");
+                string marks = Console.ReadLine();
+                int m = int.Parse(marks);
+
+                sum = sum + m; 
+            }
+            int avg = sum/ n;
+            Console.Write($"\nAverage Result of class is {avg}\n");
+
+        }
+    }
+
+}
