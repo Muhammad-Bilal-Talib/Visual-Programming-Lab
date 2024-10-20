87c5d3675588cd81b8bbd9fab7852b6c30c0c835
Muhammad-Bilal-Talib <233543@students.au.edu.pk>
2024-10-20 12:36:28 +0500

    Add files via upload

---
+++ lab-05/LAB-04/Task-06 Lab-04/Ex06 lab04/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace marks
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+            int[,] testMarks = new int[5, 10]; 
+            double[] studentAverages = new double[10]; 
+
+            
+            Console.WriteLine("Enter the test marks for 5 courses and 10 students:");
+
+            for (int course = 0; course < 5; course++) 
+            {
+                Console.WriteLine($"Entering marks for Course {course + 1}:");
+                for (int student = 0; student < 10; student++) 
+                {
+                    Console.Write($"Enter marks for Student {student + 1}: ");
+                    testMarks[course, student] = int.Parse(Console.ReadLine());
+                }
+            }
+
+     
+            for (int student = 0; student < 10; student++)
+            {
+                int totalMarks = 0;
+                for (int course = 0; course < 5; course++)
+                {
+                    totalMarks += testMarks[course, student]; 
+                }
+                studentAverages[student] = totalMarks / 5.0; 
+            }
+
+           
+            Console.WriteLine("\nTest Marks for each course and student with averages:");
+            for (int course = 0; course < 5; course++)
+            {
+                Console.Write($"Course {course + 1}: ");
+                for (int student = 0; student < 10; student++)
+                {
+                    Console.Write(testMarks[course, student] + "\t");
+                }
+                Console.WriteLine(); 
+            }
+
+         
+            Console.Write("\nAverage :");
+            for (int student = 0; student < 10; student++)
+            {
+                Console.Write($"{studentAverages[student]}    ");
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ lab-05/LAB-04/Task-07 Lab-04/Ex07 lab04/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace multiplearray
+    {
+class Program
+{
+    static void Main()
+    {
+        // Step 1: Input two 3x3 matrices
+        int[,] matrix_1 = new int[3, 3];
+        int[,] matrix_2 = new int[3, 3];
+        int[,] Result = new int[3, 3];
+
+        Console.WriteLine("Enter elements of matrix:");
+        Input_Matrix(matrix_1);
+
+        Console.WriteLine("Enter elements of matrix:");
+        Input_Matrix(matrix_2);
+
+       
+        Multiply_Matrices(matrix_1, matrix_2, Result);
+
+       
+        Console.WriteLine("Resultant Matrix:");
+        Display_Matrix(Result);
+    }
+
+    static void Input_Matrix(int[,] matrix)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"Enter element [{i},{j}]: ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+
+
+    static void Multiply_Matrices(int[,] A, int[,] B, int[,] result)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                result[i, j] = 0; 
+                for (int k = 0; k < 3; k++)
+                {
+                    result[i, j] += A[i, k] * B[k, j];
+                }
+            }
+        }
+    }
+
+
+    static void Display_Matrix(int[,] matrix)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
+}
+
