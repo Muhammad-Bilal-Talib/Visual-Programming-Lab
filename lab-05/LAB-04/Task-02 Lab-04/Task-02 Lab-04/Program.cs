514ca583f5e8a335765f2d76eeb95c862e9c3917
Muhammad-Bilal-Talib <233543@students.au.edu.pk>
2024-10-20 12:31:48 +0500

    Lab-04
    Uploaded

---
+++ lab-05/LAB-04/Task-02 Lab-04/Task-02 Lab-04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex_02_Lab04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = { "Hello ", "and ", "Welcome ", "to ", "this ", "Demo!" };
+            Console.WriteLine(merge_array(s));
+        }
+        private static string merge_array(string[] s)
+        {
+            string m = string.Concat(s);
+            return m;
+        }
+    }
+}
