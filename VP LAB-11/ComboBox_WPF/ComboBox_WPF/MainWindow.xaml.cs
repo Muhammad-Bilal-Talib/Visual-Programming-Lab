7f2adff403dd87c1126dab41fb58bda079be8c00
Muhammad-Bilal-Talib <233543@students.au.edu.pk>
2024-12-11 14:45:29 +0500

    Add files via upload

---
+++ VP LAB-11/ComboBox_WPF/ComboBox_WPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboBox_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedItem is ComboBoxItem item)
+            {
+                textBox.Text = item.Content.ToString(); 
+            }
+                
+            
+        }
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedItem is ComboBoxItem item)
+                textBox1.Text = item.Content.ToString();
+        }
+    }
+}
